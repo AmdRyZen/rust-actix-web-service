ba86f6576d867743b8954e9141662aaf059d53dc
stefano <stefano@email996.com>
2020-08-15 23:34:34 +0800

    as SearchList

--- src/http/es.rs
+++ src/http/es.rs
@@ -1,15 +1,18 @@
-use crate::http::response;
+use crate::http::{response};
 use actix_web::{web, Responder};
 use elasticsearch::*;
+extern crate serde_json;
 use serde_json::{json,Value};
+use elasticsearch::{Elasticsearch, SearchParts};
+use serde::{Deserialize, Serialize};
 
 pub async fn index(
     es_client: web::Data<Elasticsearch>
 ) -> impl Responder {
     let ret = es_client
-        .index(IndexParts::IndexId("tweets", "1"))
+        .index(IndexParts::IndexId("tweets", "2"))
         .body(json!({
-        "id": 1,
+        "id": 2,
         "user": "kimchy",
         "post_date": "2009-11-15T00:00:00Z",
         "message": "Trying out Elasticsearch, so far so good?"
@@ -27,6 +30,11 @@ pub async fn index(
     })
 }
 
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct SearchList {
+    _id: String,
+    _index: String,
+}
 pub async fn search(
     es_client: web::Data<Elasticsearch>
 ) -> impl Responder {
@@ -43,18 +51,27 @@ pub async fn search(
     }))
         .send()
         .await;
-    let response_body = ret.is_ok();
-    // let took = response_body["took"].as_i64().unwrap();
-    // for hit in response_body["hits"]["hits"].as_array().unwrap() {
-    //     // print the source document
-    //     println!("{:?}", hit["_source"]);
-    // }
 
-    println!("{:?}", response_body);
+    let response_body = match ret {
+        Ok(ret) => ret.json::<Value>().await,
+        Err(err) => Err(err.into()),
+    };
+
+    let mut data :Vec<SearchList> = vec![];
+    for i in &response_body {
+        for d in i["hits"]["hits"].as_array().unwrap() {
+            let p = SearchList {
+                _id: d["_id"].to_string(),
+                _index: d["_index"].to_string(),
+            };
+            data.push(p);
+        }
+    }
+    //println!("{:?}", data);
 
     web::Json(response::Success {
         code: response::HTTP_OK,
         message: response::HTTP_MSG.to_string(),
-        result: 1,
+        result: data,
     })
 }
