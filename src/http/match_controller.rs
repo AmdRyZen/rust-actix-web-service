acd7d8f0e6be32601597a281d8d4cbe47471d789
stefano <stefano@email996.com>
2020-08-29 22:12:52 +0800

    分层设计

--- src/http/match_controller.rs
+++ src/http/match_controller.rs
@@ -5,11 +5,16 @@ use crate::service::*;
 
 pub async fn match_list(_pool: web::Data<mysql::Pool>, _req: HttpRequest) -> impl Responder {
 
-    let match_list = Match::list().await;
+    let (count,list) = Match::list(_pool, _req).await;
 
     web::Json(response::Success {
         code: response::HTTP_OK,
         message: response::HTTP_MSG.to_string(),
-        result: match_list,
+        result: response::Result {
+            page: 1,
+            size: 10,
+            count: count,
+            list: list,
+        },
     })
 }
--- src/service/match_service.rs
+++ src/service/match_service.rs
@@ -1,15 +1,66 @@
 use serde::{Deserialize, Serialize};
+use actix_web::{web, HttpRequest};
+use mysql::prelude::*;
+use mysql::*;
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Match {
     pub id: i32,
+    pub status: i32,
+    pub name: String,
+    pub pull_url: String,
+    pub server_name: String,
+    pub created_at: String,
+    pub _type: i32,
 }
 
 impl Match
 {
-     pub async fn list() -> Self {
-         Match {
-             id: 111,
-         }
+     pub async fn list(_pool: web::Data<mysql::Pool>, _req: HttpRequest) -> (u64, Vec<Match>) {
+         let id = _req.match_info().get("id").unwrap_or("1");
+
+         let mut sql = String::from("");
+         let mut sql_where = String::from("");
+         let mut sql_count = String::from("");
+
+         sql_where.push_str(" where id >= ");
+         sql_where.push_str(&id.to_string());
+
+         sql.push_str(
+             "select id, status, name, pull_url, server_name, created_at, type from t_media_screenshot ",
+         );
+         sql.push_str(&sql_where);
+         sql.push_str(" order by id desc");
+
+         sql_count.push_str("select count(1) as total from t_media_screenshot ");
+         sql_count.push_str(&sql_where);
+
+         let mut conn = _pool.get_conn().unwrap();
+
+         let total: Result<Option<u64>> = conn.query_first(sql_count);
+         let count: u64 = match total {
+             Ok(total) => total.unwrap(),
+             Err(_e) => 0,
+         };
+
+         let result = conn.query_map(
+             sql,
+             |(id, status, name, pull_url, server_name, created_at, _type)| Match {
+                 id,
+                 status,
+                 name,
+                 pull_url,
+                 server_name,
+                 created_at,
+                 _type,
+             },
+         );
+
+         let list = match result {
+             Ok(result) => result,
+             Err(_e) => vec![],
+         };
+
+         (count, list)
     }
 }
